b58aafb20dd956b9de75c6c5d7fdd9eeaeb109ac
karimobegi <karimobegi2007@gmail.com>
2026-02-21 22:52:48 +0000

    finished the dashboard page

--- src/Website/dashboard.js
+++ src/Website/dashboard.js
@@ -1,6 +1,9 @@
 API_BASE = window.location.origin;
-
-function renderDashboard(summary){
+let chart1 = null;
+let chart2 = null;
+let chart3 = null;
+let chart4 = null;
+function monthlyChart(summary){
     const monthly = summary.monthly || [];
 
   if (monthly.length === 0) {
@@ -8,7 +11,7 @@ function renderDashboard(summary){
     return;
   }
    if (chart1) chart1.destroy();
-new Chart(document.getElementById("chart1"), {
+    chart1 = new Chart(document.getElementById("chart1"), {
     type: 'bar',
     data: {
       labels: monthly.map(row => row.month),
@@ -33,10 +36,79 @@ new Chart(document.getElementById("chart1"), {
     options: {responsive: true, maintainAspectRatio: false }
   });
 
+}
+
+function categoryChart(summary){
+    const category = summary.category_spend || [];
+    if (category.length === 0){
+        return;
+    }
+    if (chart2) chart2.destroy();
+    chart2 = new Chart(document.getElementById("chart2"), {
+    type: 'doughnut',
+    data: {
+      labels: category.map(row => row.category),
+      datasets: [{
+        label: 'Spending by Category',
+        data: category.map(row => row.amount),
+        borderWidth: 1
+      },
+    ]
+    },
+    options: {responsive: true, maintainAspectRatio: false }
+  });
+};
+
+function merchantChart(summary){
+    const merchant = summary.top_merchants;
+    if (merchant.length === 0){
+        return;
+    }
+    if (chart3) chart3.destroy();
+    chart3 = new Chart(document.getElementById("chart3"), {
+    type: 'bar',
+    data: {
+      labels: merchant.map(row => row.merchant),
+      datasets: [{
+        label: 'Merchants',
+        data: merchant.map(row => row.amount),
+        borderWidth: 1
+      }
+    ]
+    },
+    options: { indexAxis : "y", responsive: true, maintainAspectRatio: false }
+  })
 
 }
 
+function transactionChart(summary){
+    const transactions = summary.large_transactions;
+    if (transactions.length === 0){
+        return;
+    }
+    if (chart4) chart4.destroy();
+    chart4 = new Chart(document.getElementById("chart4"), {
+    type: 'bar',
+    data: {
+      labels: transactions.map(row => (row.date, row.merchant)),
+      datasets: [{
+        label: 'Transactions',
+        data: transactions.map(row => row.amount),
+        borderWidth: 1
+      }
+    ]
+    },
+    options: { indexAxis : "y", responsive: true, maintainAspectRatio: false }
+  })
+}
 
+function renderDashboard(summary){
+    monthlyChart(summary);
+    categoryChart(summary);
+    merchantChart(summary);
+    transactionChart(summary);
+
+}
 
 async function loadDashboard() {
     try {
@@ -47,10 +119,10 @@ async function loadDashboard() {
         }
 
         const summary = await res.json();
-
         renderDashboard(summary);
 
     } catch (error) {
         console.error(error);
     }
 }
+document.addEventListener("DOMContentLoaded", loadDashboard);
